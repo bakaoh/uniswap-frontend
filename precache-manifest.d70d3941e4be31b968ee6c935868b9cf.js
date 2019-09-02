6e2f5a4f06a3d5b0572bce4ddd1ef99617781383
bakaking <tatattai@yahoo.com>
2019-09-02 11:30:20 +0700

    Updates

---
+++ precache-manifest.d70d3941e4be31b968ee6c935868b9cf.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b4f3f3e63cd71d95ed1b833819ea92",
+    "url": "/uniswap-frontend/index.html"
+  },
+  {
+    "revision": "61c1f07d4b329d48731d",
+    "url": "/uniswap-frontend/static/css/0.07de6c03.chunk.css"
+  },
+  {
+    "revision": "70f2c3aa2501584e89de",
+    "url": "/uniswap-frontend/static/css/4.d75e0ccb.chunk.css"
+  },
+  {
+    "revision": "61c1f07d4b329d48731d",
+    "url": "/uniswap-frontend/static/js/0.0753442d.chunk.js"
+  },
+  {
+    "revision": "fbcbf4375f1cb47e84fd",
+    "url": "/uniswap-frontend/static/js/1.2953383a.chunk.js"
+  },
+  {
+    "revision": "fa026b4b6400d45f22c6",
+    "url": "/uniswap-frontend/static/js/10.2dd402bb.chunk.js"
+  },
+  {
+    "revision": "70f2c3aa2501584e89de",
+    "url": "/uniswap-frontend/static/js/4.71f03a3a.chunk.js"
+  },
+  {
+    "revision": "266b5ebcb021cebda313",
+    "url": "/uniswap-frontend/static/js/5.1c7042e5.chunk.js"
+  },
+  {
+    "revision": "d7045a9f32a9ee3d93f0",
+    "url": "/uniswap-frontend/static/js/6.4da6426d.chunk.js"
+  },
+  {
+    "revision": "a42f94327a59e1aa0226",
+    "url": "/uniswap-frontend/static/js/7.15414f12.chunk.js"
+  },
+  {
+    "revision": "cfd078b75d043135485e",
+    "url": "/uniswap-frontend/static/js/8.8cc6031e.chunk.js"
+  },
+  {
+    "revision": "4b89618eb16bdd630aa8",
+    "url": "/uniswap-frontend/static/js/9.9a4abefd.chunk.js"
+  },
+  {
+    "revision": "39d2c06edfea9ae56ee6",
+    "url": "/uniswap-frontend/static/js/main.fb2dbe86.chunk.js"
+  },
+  {
+    "revision": "819e9b2d1af2dcd56818",
+    "url": "/uniswap-frontend/static/js/runtime~main.9308ead4.js"
+  },
+  {
+    "revision": "ed2a1dad16cb9a4b9afd788ddaae7290",
+    "url": "/uniswap-frontend/static/media/circle-grey.ed2a1dad.svg"
+  },
+  {
+    "revision": "2d975615c4c409c3b6b00e8ae7c5767a",
+    "url": "/uniswap-frontend/static/media/circle.2d975615.svg"
+  },
+  {
+    "revision": "b20914ec5482543a0b1b2c6d5509ab96",
+    "url": "/uniswap-frontend/static/media/dropdown-blue.b20914ec.svg"
+  },
+  {
+    "revision": "7d32d2fa19d17d6ab9f0e0067bebaf96",
+    "url": "/uniswap-frontend/static/media/dropdown.7d32d2fa.svg"
+  },
+  {
+    "revision": "b96d70e10dd30a64a0d122603577c8ae",
+    "url": "/uniswap-frontend/static/media/dropup-blue.b96d70e1.svg"
+  },
+  {
+    "revision": "802c6eacbb13c83b27c39be505228231",
+    "url": "/uniswap-frontend/static/media/ethereum-logo.802c6eac.svg"
+  },
+  {
+    "revision": "674400972753804891ec372652944539",
+    "url": "/uniswap-frontend/static/media/magnifying-glass.67440097.svg"
+  },
+  {
+    "revision": "e8021e51723d455b2f9fa2446808d2a3",
+    "url": "/uniswap-frontend/static/media/plus-blue.e8021e51.svg"
+  },
+  {
+    "revision": "cc0a245174670956764c52ccc9238c08",
+    "url": "/uniswap-frontend/static/media/question.cc0a2451.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/uniswap-frontend/static/media/x.b0200bf7.svg"
+  }
+]);
